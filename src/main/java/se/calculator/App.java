103a2f090bcdc2a61939857e35a148bc3ca0cbf6
swe2mohamed <swe.mohamed1990@gmail.com>
2024-02-29 13:12:40 +0100

    Fix the wrong

--- src/main/java/se/calculator/App.java
+++ src/main/java/se/calculator/App.java
@@ -1,5 +1,6 @@
 package se.calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App 
@@ -20,58 +21,64 @@ public class App
             Scanner userFirstNum = new Scanner(System.in);
             Scanner userSecondNum = new Scanner(System.in);
             Scanner scannerOperation = new Scanner(System.in);
-            //__operation
+            //__
+            try {
             System.out.print("The number of operation: ");
             int operation = scannerOperation.nextInt();
-            if (operation < 1 || operation > 5) {
+
+                if (operation < 1 || operation > 5) {
+                    System.out.println("**************************");
+                    System.out.println("<<<<< Invalid >>>>>");
+                }
+                else if ( operation == 5) {
+                    isNotExit = false;
+                    System.out.println("The operation is exit.");
+                }
+                else{
+                        System.out.print("Write the 1th number: ");
+                        long firstNum = userFirstNum.nextInt();
+                        System.out.print("Write the 2th number: ");
+                        long secondNum = userSecondNum.nextInt();
+                        //__
+                        long addition = addition(firstNum, secondNum);
+                        long subtraction = subtraction(firstNum, secondNum);
+                        long multiplication = multiplication(firstNum, secondNum);
+                        long division = division(firstNum, secondNum);
+                        //__
+                        if (operation == 1){
+                            System.out.println("Result: " + addition);
+                        }
+                        else if (operation == 2){
+                            System.out.println("Result: " + subtraction);
+                        }
+                        else if (operation == 3){
+                            System.out.println("Result: " + multiplication);
+                        }
+                        else{
+                            System.out.println("Result: " + division);
+                        }
+                }
+            }catch (InputMismatchException exception){
                 System.out.println("**************************");
-                System.out.println("<<<<< Invalid >>>>>");
-            }
-            else if ( operation == 5) {
-                isNotExit = false;
-                System.out.println("The operation is exit.");
-            }
-            else{
-                    System.out.print("Write the 1th number: ");
-                    int firstNum = userFirstNum.nextInt();
-                    System.out.print("Write the 2th number: ");
-                    int secondNum = userSecondNum.nextInt();
-                    //__
-                    int addition = addition(firstNum, secondNum);
-                    int subtraction = subtraction(firstNum, secondNum);
-                    int multiplication = multiplication(firstNum, secondNum);
-                    int division = division(firstNum, secondNum);
-                    //__
-                    if (operation == 1){
-                        System.out.println("Result: " + addition);
-                    }
-                    else if (operation == 2){
-                        System.out.println("Result: " + subtraction);
-                    }
-                    else if (operation == 3){
-                        System.out.println("Result: " + multiplication);
-                    }
-                    else if (operation == 4){
-                        System.out.println("Result: " + division);
-                    }
+                System.out.println(">>>>> Wrong message: The input not an integer or To Big");
             }
         }
     }
 
 
-    public static int addition(int a,int b){
+    public static long addition(long a,long b){
         return a + b;
     }
 
-    public  static int subtraction(int a, int b){
+    public  static long subtraction(long a, long b){
         return a - b;
     }
 
-    public static int multiplication(int a, int b){
+    public static long multiplication(long a, long b){
         return a * b;
     }
 
-    public static  int division(int a, int b){
+    public static  long division(long a, long b){
         return a/b;
     }
 }
