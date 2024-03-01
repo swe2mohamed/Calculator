10e718334b1d4c151427736fd1db4f0e4ba29f01
swe2mohamed <swe.mohamed1990@gmail.com>
2024-03-01 06:22:58 +0100

    handle zero division

--- src/main/java/se/calculator/App.java
+++ src/main/java/se/calculator/App.java
@@ -3,10 +3,11 @@ package se.calculator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
         boolean isNotExit = true;
         while(isNotExit){
             System.out.println("*********************");
@@ -18,15 +19,13 @@ public class App
             System.out.println("4- Division.");
             System.out.println("5- Exit.");
 
-            Scanner scanner = new Scanner(System.in);
-
             //__
             try {
-            System.out.print("The number of operation: ");
-            int operation = scanner.nextInt();
+                System.out.print("The number of operation: ");
+                int operation = scanner.nextInt();
 
                 if (operation < 1 || operation > 5) {
-                    System.out.println("**************************");
+                    System.out.println("__________________");
                     System.out.println("<<<< Invalid >>>>");
                 }
                 else if ( operation == 5) {
@@ -34,34 +33,37 @@ public class App
                     System.out.println("The operation is exit.");
                 }
                 else{
-                        System.out.print("Write the 1th number: ");
-                        long firstNum = scanner.nextInt();
-                        System.out.print("Write the 2th number: ");
-                        long secondNum = scanner.nextInt();
-                        //__
-                        long addition = addition(firstNum, secondNum);
-                        long subtraction = subtraction(firstNum, secondNum);
-                        long multiplication = multiplication(firstNum, secondNum);
-                        long division = division(firstNum, secondNum);
-                        //__
-                        if (operation == 1){
-                            System.out.println("Result: " + addition);
-                        }
-                        else if (operation == 2){
-                            System.out.println("Result: " + subtraction);
-                        }
-                        else if (operation == 3){
-                            System.out.println("Result: " + multiplication);
-                        }
-                        else{
-                            System.out.println("Result: " + division);
-                        }
+                    System.out.print("Write the 1st number: ");
+                    long firstNum = scanner.nextInt();
+                    System.out.print("Write the 2nd number: ");
+                    long secondNum = scanner.nextInt();
+                    //__
+                    long addition = addition(firstNum, secondNum);
+                    long subtraction = subtraction(firstNum, secondNum);
+                    long multiplication = multiplication(firstNum, secondNum);
+                    double division = division((double) firstNum, (double)secondNum);
+                    //__
+                    if (operation == 1){
+                        System.out.println("Result: " + addition);
+                    }
+                    else if (operation == 2){
+                        System.out.println("Result: " + subtraction);
+                    }
+                    else if (operation == 3){
+                        System.out.println("Result: " + multiplication);
+                    }
+                    else{
+                        System.out.println("Result: " + division);
+                    }
                 }
             }catch (InputMismatchException exception){
                 System.out.println("**************************");
-                System.out.println(">>>>> Wrong message: The input not an integer or To Big");
+                System.out.println("Input error: The input is not an integer or is too big");
+                scanner.next();
             }
+
         }
+        scanner.close();
     }
 
 
@@ -77,8 +79,14 @@ public class App
         return a * b;
     }
 
-    public static  long division(long a, long b){
-        return a/b;
+    public static  double division(double a, double b){
+        if (b == 0){
+            System.out.println("You can not divide by zero.");
+            return Double.NaN;
+        }
+        else {
+            return a / b;
+        }
     }
 }
 
